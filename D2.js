9bf36e1222b90ecbbcc7f6c824abf6f42d6d8c26
Saverio Caracappa <saveriocaracappa@gmail.com>
2022-12-28 19:21:26 +0100

    do more exercise

--- D2.js
+++ D2.js
@@ -12,34 +12,37 @@ REGOLE
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let numero = 1;
-//la variabile è quella memoria temporanea a cui attribuiamo una stringa o un valore numerico (sia intero che decimale) o un valore booleano.
-console.log(numero);
+
 
 /* ESERCIZIO 2
  Crea una variable chiamata "name" e assegna ad essa il tuo nome, sotto forma di stringa.
 */
 
-/* SCRIVI QUI LA TUA RISPOSTA */
-let nome = "Saverio";
-console.log(nome);
+console.log("---------ESERCIZIO 2---------")
+let Name = "Saverio"
+console.log(Name)
 
 /* ESERCIZIO 3
  Scrivi il codice necessario ad effettuare un addizione (una sottrazione) dei numeri 12 e 20.
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let numero1 = 12;
-let numero2 = 20;
-let addizione = numero1 + numero2;
-console.log(addizione);
+console.log("---------ESERCIZIO 3---------")
+const num1 = 12;
+const num2 = 20
+const risultato = num1 + num2
+console.log(risultato)
 
 /* ESERCIZIO 4
  Crea una variable di nome "x" e assegna ad essa il numero 12.
 */
 
+
 /* SCRIVI QUI LA TUA RISPOSTA */
-let x = 12;
+console.log("---------ESERCIZIO 4---------")
+const x = 12 
+console.log(x)
+
 
 /* ESERCIZIO 5
   Riassegna un nuovo valore alla variabile "name" già esistente: il tuo cognome.
@@ -47,19 +50,20 @@ let x = 12;
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-//const nome = "caracappa";
-//console.log(nome);
-//naturalmente si avrà errore perché una un valore con una variabile non può essere nuovamente dichiarata una seconda volta
+console.log("---------ESERCIZIO 5---------")
+//const Name = "Caracappa"
+console.log("Non è possibile riassegnare una variabile assegnata già in precedenza")
+
 
 /* ESERCIZIO 6
  Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12).
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let numero3 = 4;
+console.log("---------ESERCIZIO 6---------")
+const result = 4 - x
+console.log(result)
 
-let sottrazione = numero3 - x;
-console.log(sottrazione);
 /* ESERCIZIO 7
  Crea due variabili: "name1" e "name2". Assegna a name1 la stringa "john", e assegna a name2 la stringa "John" (con la J maiuscola!).
  Verifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa).
@@ -67,13 +71,19 @@ console.log(sottrazione);
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-let name1 = "john";
-let name2 = "John";
-console.log(name1 === name2);
-console.log(name1.toLowerCase() === name2.toUpperCase());
+console.log("---------ESERCIZIO 7---------")
 
-//if (name2.toLowerCase) {
-//  block of code to be executed if condition1 is true
-//} else if (name2.toUpperCase) {
-//  block of code to be executed if the condition1 is false and condition2 is true
-//}
+let name1 = "john"
+let name2 = "John"
+let uguaglianza =  name1 == name2  
+console.log(uguaglianza)
+
+
+if (name1 === name2.toLowerCase())  {
+  true
+  
+ } 
+ 
+  console.log("name1 è uguale a name2 se entrambi in lower?" )
+  
+  
